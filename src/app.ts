6bd59b2827f5519d64829b84f3f2a3f3fbccdf54
dixonAnatoAscencio <dixonjesusanato@gmail.com>
2024-05-30 22:49:16 -0500

    add webhooks

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import { envs } from "./config";
+import express from "express";
+import { GithubController } from "./presentation/github/controller";
+
+(() => {
+  main();
+})();
+
+function main() {
+  const app = express();
+  const controller = new GithubController();
+
+  app.use(express.json());
+
+  app.post("/api/github", controller.webhookHandler )
+
+  app.listen(envs.PORT, () => {
+    console.log(`Server is running on port ${envs.PORT}`);
+  });
+}
